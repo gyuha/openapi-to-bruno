4f56806f91111bc755317137f90fb3ca41376790
Gyuha <nicegyuha@gmail.com>
2024-07-05 14:00:28 +0900

    update

--- example/run.bat
+++ example/run.bat
@@ -1,1 +1,2 @@
-npx  --verbose github:gyuha/openapi-to-bruno -s https://petstore.swagger.io/v2/swagger.json -o ./output
+@rem npx --verbose github:gyuha/openapi-to-bruno -s https://petstore.swagger.io/v2/swagger.json -o ./output
+npx github:gyuha/openapi-to-bruno -s https://petstore.swagger.io/v2/swagger.json -o ./output
